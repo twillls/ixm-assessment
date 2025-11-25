614da20145d425e6948b8d407aff6d71b7869527
Taylor Wills <tay.dronfield@gmail.com>
2025-11-25 15:37:55 -0500

    Site scaffolding for menus, layout, landing page ct, hero paragraph

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -900,3 +900,9 @@ require DRUPAL_ROOT . "/../vendor/acquia/drupal-recommended-settings/settings/ac
  *
  * @link https://docs.acquia.com/
  */
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
